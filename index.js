51aa295b70a1ac80f0db454190af2f507c99d72c
zippy0626 <zippy0626@gmail.com>
2024-11-08 11:49:56 -0500

    solved ch5

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ function addGamesToPage(games) {
             <img src="${game.img}" class="game-img" />
             <p>${game.description}</p>
             <p>Backers: ${game.backers}</p>
+            <p>Pledged: $${game.pledged.toLocaleString("en-US")}</p>
+            <p>Goal: $${game.goal.toLocaleString("en-US")}</p>
         `;
     gamesContainer.appendChild(gameCard);
   }
@@ -83,25 +85,29 @@ gamesCard.innerHTML = `
 function filterUnfundedOnly() {
   deleteChildElements(gamesContainer);
 
-  // use filter() to get a list of games that have not yet met their goal
+  let unfundedGames = GAMES_JSON.filter((game) => {
+    return game.pledged < game.goal
+  });
 
-  // use the function we previously created to add the unfunded games to the DOM
+  addGamesToPage(unfundedGames);
 }
 
 // show only games that are fully funded
 function filterFundedOnly() {
   deleteChildElements(gamesContainer);
 
-  // use filter() to get a list of games that have met or exceeded their goal
+  let fundedGames = GAMES_JSON.filter((game) => {
+    return game.pledged >= game.goal
+  });
 
-  // use the function we previously created to add unfunded games to the DOM
+  addGamesToPage(fundedGames);
 }
 
 // show all games
 function showAllGames() {
   deleteChildElements(gamesContainer);
 
-  // add all games from the JSON data to the DOM
+  addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -109,7 +115,9 @@ const unfundedBtn = document.getElementById("unfunded-btn");
 const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
-// add event listeners with the correct functions to each button
+unfundedBtn.addEventListener('click', filterUnfundedOnly);
+fundedBtn.addEventListener('click', filterFundedOnly);
+allBtn.addEventListener('click', showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
